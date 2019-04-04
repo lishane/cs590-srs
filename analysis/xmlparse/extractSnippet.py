4daef979956b0b2016d03859538ba18f71c08475
potatoshane <li2137@purdue.edu>
2019-04-04 12:09:41 -0400

    Fix bug for xml parse

--- analysis/xmlparse/extractSnippet.py
+++ analysis/xmlparse/extractSnippet.py
@@ -5,8 +5,10 @@ with open('Posts.xml', mode='r', buffering=1) as f:
     with open('PostsSnippet.xml', 'w') as w:
         i = 0
         line = f.readline()
-        if 'android' in line:
-            lines.append(line)
-            print i
-            i += 1
-            w.write(line)
+        while line:
+            if 'android' in line:
+                lines.append(line)
+                print i
+                i += 1
+                w.write(line)
+                line = f.readline()
